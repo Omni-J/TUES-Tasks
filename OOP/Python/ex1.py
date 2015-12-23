d84949dccdc3343878efed80104b2944f46c1088
BassMuff <funkmetaljazzmaster@gmail.com>
2015-12-23 12:37:20 +0200

    Python basics

---
+++ OOP/Python/ex1.py
@@ -0,0 +1,21 @@
+
+
+i = 1.0
+print(i)
+print("Hello world!")
+
+
+print(type(i))
+
+a = "Hello"
+print(type(a))
+
+# print(a+i)  <-Error!
+
+a += "world!"
+
+print(a)
+
+print("this is string: {}, {}".format(3, "ala bala"))
+print("pi = %f" % 3.14)
+
