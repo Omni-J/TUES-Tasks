a8a00cb866c4be05c3272cdebe75a57a4118d216
Julian Stoev <xxthespinerxx@gmail.com>
2016-02-23 18:15:30 +0200

    Added WEB notes

---
+++ WEB/23.02.16jsbasics/array.js
@@ -0,0 +1,20 @@
+var array = ["Sup","Nigga"];
+array[0]; // Sup
+array[1]; // Nigga
+array.push(value);
+array.pop();
+array.unshift(value); // at the start
+array.shift(); // pops at the start
+array.sort();
+array.split();
+array.join();
+// JS is event driven, nqma main
+function my_funk(){
+  statement;
+  statement;
+}
+<button onclick="Myfunk()">Click me </button> // ako e prosto Myfunk bez skobi ne e fynk a property
+
+//js gives the activity/dynamity of the sites
+//get elements by id
+// inner html, html and outer html
